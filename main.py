610ce73893dabd9f13759362d5a7d95c9e47824c
sreejith <squeeky.mouse@gmail.com>
2023-03-13 09:53:53 +0530

    PyQt main window and menubar Sec46

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from PyQt6.QtWidgets import QApplication, QLabel,QWidget,QGridLayout,\
+    QLineEdit,QPushButton,QMainWindow
+from PyQt6.QtGui import QAction
+import sys
+# commit: PyQt main window and menubar Sec46
+
+#QMainWindow provides menu bar status bar and stuff!!!
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('Student Management System')
+
+        # menu items
+        file_menu_item=self.menuBar().addMenu('&File')
+        help_menu_item=self.menuBar().addMenu('&Help')
+
+        add_student_action=QAction('Add Student',self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action=QAction('About',self)
+        help_menu_item.addAction(about_action)
+        # about_action.setMenuRole(QAction.MenuRole.NoRole) # needed on mac if help menu is not shown!!!
 
 
-# commit: initial commit Sec45
+
+
+app=QApplication(sys.argv)
+age_calculator=MainWindow()
+age_calculator.show()
+sys.exit(app.exec())
